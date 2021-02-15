bf1526f27aebb7f6cc38f0187b13e0d01d16ecc4
Christiaan Hemerik <Christiaan.Hemerik@thelearningnetwork.nl>
2021-02-15 16:02:52 +0100

    Updates

---
+++ precache-manifest.62fc3979f4999fb2a84e68aa234ae8d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be22efa4f92ee7c13563bcb8880fc52",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5d6f1d2111cad15a3d4e.bundle.js"
+  },
+  {
+    "url": "runtime~main.5d6f1d2111cad15a3d4e.bundle.js"
+  },
+  {
+    "revision": "a0d1a5baa9d2ff7ecc43",
+    "url": "static/css/main.96dc4647.chunk.css"
+  },
+  {
+    "revision": "d00a968e744d3b8f01b83f5eba468803",
+    "url": "static/media/app-example-01.d00a968e.png"
+  },
+  {
+    "revision": "773203f87b5ff71f538bddf06a4675c2",
+    "url": "static/media/article-example-01.773203f8.png"
+  },
+  {
+    "url": "vendors~main.5d6f1d2111cad15a3d4e.bundle.js"
+  },
+  {
+    "url": "vendors~main.5d6f1d2111cad15a3d4e.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.0c930d67e30aea54c83ccab8fcfbd4e2.js"
+  "precache-manifest.62fc3979f4999fb2a84e68aa234ae8d0.js"
 );
 
 self.addEventListener('message', (event) => {
